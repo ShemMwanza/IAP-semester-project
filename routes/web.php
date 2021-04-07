cb9639f8c14fc1c21c5d12914b4b6b41c9e2f3c8
Georgina7 <57445279+Georgina7@users.noreply.github.com>
2021-04-07 11:06:04 +0300

    Conflict Resolve 5

--- routes/web.php
+++ routes/web.php
@@ -34,11 +34,8 @@ Route::post('artist/changePassword',[MainController::class,'changePassword']);
 Route::post('artist/addCraft',[MainController::class,'addCraft']);
 Route::post('artist/editCraft',[MainController::class,'editCraft']);
 Route::post('artist/craftUpdate',[MainController::class,'craftUpdate']);
-Route::post('artist/deleteCraft',[MainController::class,'deleteCraft']);
-<<<<<<< HEAD
+Route::post('artist/deleteCraft',[MainController::class,'deleteCraft'])
 Route::get('auth/reset_password',[MainController::class,'getResetPasswordPage'])->middleware('guest');
 Route::post('auth/reset_password',[MainController::class,'resetPassword'])->middleware('guest')->name('password.reset');
-=======
 Route::get('artist/searchArtist',[MainController::class,'searchArtist']);
 Route::get('artist/findArtist', [MainController::class,'getFindArtistPage']);
->>>>>>> 9bc67998437dde58c89a7ead99e95ecf65968f83
