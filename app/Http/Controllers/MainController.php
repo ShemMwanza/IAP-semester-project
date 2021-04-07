a1dd5f846b3aa5ce7a3b2037781b1a0ce43d7019
Georgina7 <57445279+Georgina7@users.noreply.github.com>
2021-04-07 09:44:55 +0300

    Resolve Conflict 4

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\Art;
+use DB;
 class MainController extends Controller
 {
     protected $password;
